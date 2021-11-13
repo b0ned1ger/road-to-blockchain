9e9a1133f3d17f48c94c0acdfeb498fdea91db6a
b0ned1ger <30976199+b0ned1ger@users.noreply.github.com>
2021-11-13 10:29:23 +0200

    4.3 The Slice type

--- learning-rust/ownership/src/main.rs
+++ learning-rust/ownership/src/main.rs
@@ -202,4 +202,99 @@ fn main() {
   // Let’s recap what we’ve discussed about references:
   // * At any given time, you can have either one mutable reference or any number of immutable references.
   // * References must always be valid.
+
+  println!("===== The slice type");
+  {
+    // write a function that takes a string and returns the first word it finds in that string. 
+    // we could return the index of the end of the word
+    fn first_word(s: &String) -> usize {
+      // we’ll convert our String to an array of bytes
+      let bytes = s.as_bytes();
+      // iter is a method that returns each element in a collection
+      // enumerate wraps the result of iter and returns each element as part of a tuple instead
+      // Because the enumerate method returns a tuple, we can use patterns to destructure that tuple.
+      // So in the for loop, we specify a pattern that has i for the index in the tuple and &item 
+      // for the single byte in the tuple.
+      for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+          return i;
+        }
+      }
+      s.len()
+    }
+
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s); // word will get the value 5
+
+    s.clear(); // this empties the String, making it equal to ""
+
+    println!("{}", word);
+    // word still has the value 5 here, but there's no more string that
+    // we could meaningfully use the value 5 with. word is now totally invalid!
+    // Luckily, Rust has a solution to this problem: string slices.
+
+    // The type that signifies “string slice” is written as &str
+    fn f_word(s: &String) -> &str {
+      let bytes = s.as_bytes();
+      for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+          return &s[0..i];
+        }
+      }
+      &s[..]
+    }
+    
+    let s = String::from("hello world");
+    let word = f_word(&s);
+
+    println!("the first word is: {}", word);
+  }
+
+  println!("===== String Literals Are Slices");
+  {
+    // The type of _s here is &str: it’s a slice pointing to that specific point of the binary.
+    // This is also why string literals are immutable; &str is an immutable reference.
+    let _s = "Hello, world!";
+  }
+
+  println!("===== String Slices as Parameters");
+  {
+    fn first_word(s: &str) -> &str {
+      let bytes = s.as_bytes();
+      for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+          return &s[0..i];
+        }
+      }
+      &s[..]
+    }
+
+    let my_string = String::from("hello world");
+
+    // `first_word` works on slices of `String`s, whether partial or whole
+    let _word = first_word(&my_string[0..6]);
+    let _word = first_word(&my_string[..]);
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s
+    let _word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+
+    // `first_word` works on slices of string literals, whether partial or whole
+    let _word = first_word(&my_string_literal[0..6]);
+    let _word = first_word(&my_string_literal[..]);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let _word = first_word(my_string_literal);
+  }
+
+  println!("===== String Slices as Parameters");
+  {
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[1..3];
+
+    assert_eq!(slice, &[2, 3]);
+  }
 }
